dd02c051bbc4785ddfebb9fff07e8a2f85d14f79
Luis Acedo <lacedo@oceano.com>
2018-07-11 13:58:19 +0200

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="about">
+        <h1>About this page</h1>
+        <p>That just sounds like slavery with extra steps. Yeah, sure, I mean, if you spend all day shuffling words around, you can make anything sound bad. Nothing you do matters, your existence is a lie! I wish that shotgun was my penis.</p>
+    </div>
+</template>
+
+
